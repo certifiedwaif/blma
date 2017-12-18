1c5050bed9abd729e8372ceb621e67d507648d14
Mark Greenaway <markg@maths.usyd.edu.au>
2017-12-18 16:05:41 +1100

    Re-factor model prior parameter checks

--- src/correlation.cpp
+++ src/correlation.cpp
@@ -315,31 +315,30 @@ bool check_model_prior_parameters(const std::string modelprior, const VectorXd& 
 	if (modelprior == "uniform") {
 		return true;
 	} else if (modelprior == "beta-binomial") {
-		if (modelpriorvec.size() == 2) {
-			if (modelpriorvec(0) > 0 && modelpriorvec(1) > 0)
-				return true;
-			else {
-				invalid_reason = "elements of modelpriorvec should be positive";
-				return false;
-			}
-		}
-		else {
+		if (modelpriorvec.size() != 2) {
 			invalid_reason = "modelpriorvec was not of size 2";
 			return false;
 		}
+
+		if (modelpriorvec(0) <= 0.|| modelpriorvec(1) <= 0.) {
+			invalid_reason = "elements of modelpriorvec should be positive";
+			return false;
+		}
+
+		return true;
 	} else if (modelprior == "bernoulli") {
-		if (modelpriorvec.size() == mX.cols()) {
-			if (abs(modelpriorvec.sum() - 1.) > 1e-5) {
-				invalid_reason = "modelpriorvec does not sum to 1";
-				return false;
-			}
-			return true;
-		} else {
+		if (modelpriorvec.size() != mX.cols()) {
 			invalid_reason = "modelpriorvec was not of the same size as the number of columns in mX";
 			return false;
 		}
-	}
-	else {
+
+		if (abs(modelpriorvec.sum() - 1.) > 1e-5) {
+			invalid_reason = "modelpriorvec does not sum to 1";
+			return false;
+		}
+
+		return true;
+	} else {
 		invalid_reason = "modelprior unknown";
 		return false;
 	}
