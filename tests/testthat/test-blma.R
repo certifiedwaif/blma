ec8cef7c0c898261bcc1d9648a47044ba01dbeac
Mark Greenaway <markg@maths.usyd.edu.au>
2017-12-18 15:45:03 +1100

    Label tests, add test for bernoulli modelpriorvec summing to 1

--- tests/testthat/test-blma.R
+++ tests/testthat/test-blma.R
@@ -72,6 +72,7 @@ test_that("blma check modelprior parameters", {
   UScrime_data <- gen_UScrime()
   y.t <- UScrime_data$y.t
   X.f <- UScrime_data$X.f
-  expect_error(blma(y.t, X.f, "ZE", "beta-binomial", c(2)))
-  expect_error(blma(y.t, X.f, "ZE", "bernoulli", c(1)))
+  expect_error(blma(y.t, X.f, "ZE", "beta-binomial", c(2)), label="Wrong length, should be 2")
+  expect_error(blma(y.t, X.f, "ZE", "bernoulli", c(1)), label="Wrong length, should be p=15")
+  expect_error(blma(y.t, X.f, "ZE", "bernoulli", rep(1, 15)), label="Does not sum to 1")
 })
