01f86a5a95364a3b68fd1e88c0d75df78d2b2c40
Mark Greenaway <certifiedwaif@gmail.com>
2018-02-10 11:22:32 +1100

    Add modelprior again ...

--- src/cva.cpp
+++ src/cva.cpp
@@ -532,7 +532,7 @@ List cva(const NumericVector vy_in, const NumericMatrix mX_in,
 		#endif
 
 		#pragma omp parallel for\
-			shared(vy, mX, modelpriorvec, mGamma, gamma, log_prob, log_probs, w, mXTX_inv, sigma2, vm)\
+			shared(vy, mX, modelprior, modelpriorvec, mGamma, gamma, log_prob, log_probs, w, mXTX_inv, sigma2, vm)\
 			default(none)
 		for (auto k = 0; k < K; k++) {
 			#ifdef DEBUG
