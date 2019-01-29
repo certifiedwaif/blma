c47e563b42b41148bb56ffec4d2cb12b489a11fd
Mark Greenaway <certifiedwaif@gmail.com>
2019-01-29 14:23:56 -0800

    Loop through all data sets and priors

--- tables.R
+++ tables.R
@@ -1,2 +1,7 @@
-data.sets <- c("Kakadu", "UScrime", "comCrime", "eyeData")
+data_sets <- c("Kakadu", "UScrime", "comCrime", "eyeData")
 priors <- c("BIC", "ZE", "liang_g1", "liang_g2", "liang_g_n_approx", "liang_g_n_quad", "robust_bayarri1", "robust_bayarri2", "zellner_siow_gauss_laguerre")
+for (data_set in data_sets) {
+	for (prior in prior) {
+		cat(data_set, prior, "\n")
+	}
+}
