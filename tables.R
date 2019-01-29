a17e4af4fa67893adbbaa7fd446dcc11cdf23338
Mark Greenaway <certifiedwaif@gmail.com>
2019-01-29 13:37:16 -0800

    Start working on code to produce tables

---
+++ tables.R
@@ -0,0 +1,2 @@
+data.sets <- c("Kakadu", "UScrime", "comCrime", "eyeData")
+priors <- c("BIC", "ZE", "liang_g1", "liang_g2", "liang_g_n_approx", "liang_g_n_quad", "robust_bayarri1", "robust_bayarri2", "zellner_siow_gauss_laguerre")
