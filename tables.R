05cfe63368fa1e6352c8b11aab4066512f66d355
Mark Greenaway <certifiedwaif@gmail.com>
2019-01-30 21:36:45 +0000

    Numerous bug fixes relating to lists
    * `[]` versus `[[]]`

--- tables.R
+++ tables.R
@@ -8,7 +8,7 @@ save_table_data <- function()
 	priors <- c("BIC", "ZE", "liang_g1", "liang_g2", "liang_g_n_approx", "liang_g_n_quad", "robust_bayarri1", "robust_bayarri2", "zellner_siow_gauss_laguerre")
 	results <- list()
 	for (data_set in data_sets) {
-		results[data_set] <- list()
+		results[[data_set]] <- list()
 		for (prior in priors) {
 			cat(data_set, prior)
 			ds <- NULL
@@ -20,21 +20,21 @@ save_table_data <- function()
 			vy <- ds$vy
 			mX <- ds$mX
 			p <- ncol(mX)
- 	 		cores <- detect_cores()
+ 	 		cores <- detectCores()
  	 		# Our package is currently not thread-safe for these cases
 			if (data_set %in% c("Kakadu", "UScrime") && prior %in% c("liang_g_n_appell", "zellner_siow_gauss_laguerre")) { 
 				cores <- 1
 			}
 			tic()
 			if (data_set %in% c("Kakadu", "UScrime")) {
-				cat("exact")
+				cat(" exact")
 				modelprior <- "uniform"
-				results[data_set][prior] = blma(vy, mX, prior = prior, modelprior = modelprior, cores = cores)
+				results[[data_set]][[prior]] <- blma(vy, mX, prior = prior, modelprior = modelprior, cores = cores)
 			} else {
-				cat("sampler")
+				cat(" sampler")
 				modelprior <- "beta-binomial"
 				modelpriorvec <- c(1, p)
-				results[data_set][prior] = sampler(
+				results[[data_set]][[prior]] <- sampler(
 												vy,
 												mX,
 												prior = prior,
@@ -43,7 +43,7 @@ save_table_data <- function()
 												cores = cores
 											)
 			}
-			results[data_set][prior]["tictoc"] <- toc()
+			results[[data_set]][[prior]][["tictoc"]] <- toc()
 			save(results, file="results.RData")
 			cat("\n")
 		}
