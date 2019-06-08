995a1f165f7d4add5606002564d3fb08aada13ac
Mark Greenaway <certifiedwaif@gmail.com>
2019-06-08 11:06:34 +1000

    Generate job YAML for all jobs

--- split_tests.py
+++ split_tests.py
@@ -3,6 +3,7 @@
 from typing import List
 import re
 import glob
+from jinja2 import Template
 
 filenames: List[str] = glob.glob('tests/testthat_backup/test-*.R')
 test_fns: List[str] = []
@@ -20,13 +21,15 @@ library(parallel)
 cores <- detectCores()
 
 """
+names = []
 for test_fn in test_fns:
-    print(test_fn.split("\n")[0])
+    # print(test_fn.split("\n")[0])
     name_matches = re.match('.*["\'](.*)["\'].*', test_fn)
     if name_matches is None: continue
     name = name_matches.group(1)
     name = name.replace(' ', '_')
-    print(name)
+    names.append(name)
+    # print(name)
     open(f'tests/testthat/test-{name}.R', 'w').write(preamble + test_fn)
     # TODO: Need to include
     # library(parallel); cores <- detectCores()
@@ -34,3 +37,20 @@ for test_fn in test_fns:
     # Then
     # library(testthat)
     # test_file(f'tests/testthat/test-{name}.R')
+job_yaml_template:str = Template("""apiVersion: batch/v1
+kind: Job
+metadata:
+  name: blma
+spec:
+  template:
+    parallelism: {{ len(names) }}
+    spec:
+      containers: {% for name in names %}
+      - name: {{ name.lower().replace('_', '-') }}
+        image: certifiedwaif/blma
+        command: ["R", "-e", "testthat::test_file('tests/testthat/test-{{ name }}.R')"]
+    {% endfor %}
+      restartPolicy: Never
+  backoffLimit: 4""")
+job_yaml: str = job_yaml_template.render(names=names)
+open('job.yaml', 'w+').write(job_yaml)
