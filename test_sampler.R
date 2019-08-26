0b358195d962c17b4f73067fbce3b42a7def7183
Mark Greenaway <certifiedwaif@gmail.com>
2019-08-27 06:49:32 +1000

    Correct prior names, use correct model priors for n < p

--- test_sampler.R
+++ test_sampler.R
@@ -6,7 +6,7 @@ cores <- detectCores()
 set.seed(2019)
 
 data_sets <- c('eyeData', 'comCrime')
-priors <- c('BIC', 'hyper-g', 'robust', 'beta-prime')
+priors <- c('BIC', 'liang_g1', 'robust_bayarri1', 'zellner_siow_gauss_laguerre')
 
 call_sampler <- function(data_set, prior)
 {
@@ -21,13 +21,22 @@ call_sampler <- function(data_set, prior)
     vy <- comCrime$vy
     mX <- comCrime$mX
   }
+
+  n <- nrow(mX)
   p <- ncol(mX)
 
-  modelprior <- 'beta-binomial' 
-  modelpriorvec <- c(1, p)
+  if (n < p) {
+    modelprior <- 'beta-binomial' 
+    modelpriorvec <- c(1, p)
+  } else {
+    modelprior <-'uniform'
+    modelpriorvec <- NULL
+  }
+
   sampler(100000, vy, mX, prior=prior, modelprior=modelprior, modelpriorvec=modelpriorvec, cores=cores)
 }
 
 sampler_calls <- cross2(data_sets, priors) %>%
                    transpose %>%
                    pmap(call_sampler)
+save(sampler_calls, file='sampler_calls.rda')
